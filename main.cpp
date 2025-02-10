2a567d777144c7cdb25644ca4c099b7afa3694d6
Marco Tornambe <tornambe.marco2004@gmail.com>
2025-02-10 14:23:27 +0100

    simulazione di terra e luna attorno al sole

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include "evolve.h"
 #include "integrator.h"
 
+#include <fstream>
+
 
 using namespace std;
 
@@ -29,32 +31,36 @@ int main()
   v[1] = 1022.;
   earthMoon.add(&moon, x, v);
    
-  // body sun;
-  // sun.setName("Sun").setMass(1.9891e30);
+  body sun;
+  sun.setName("Sun").setMass(1.9891e30);
 
-  // solarSystem s;
-  // x = v = {0., 0., 0.};
-  // s.add(&sun, x, v);
-  // x[1] = 1.496e11;
-  // v[0] = -29.78e3;
-  // s.add(&earthMoon, x, v);
+  solarSystem s;
+  x = v = {0., 0., 0.};
+  s.add(&sun, x, v);
+  x[1] = 1.496e11;
+  v[0] = -29.78e3;
+  s.add(&earthMoon, x, v);
 
   // prepare for the evolution
   double totalTime = 44192000, dt = 60, G = 6.67e-11;
   evolve myEvolution;
   RK2 I;
   myEvolution.setAlgorithm(&I);
-  myEvolution.setSystem(&earthMoon);
+  myEvolution.setSystem(&s);
   myEvolution.setDt(dt);
   myEvolution.setG(G);
   int steps = (int) (totalTime/dt + 0.5);
+  cout << "# Steps: " << steps << endl;
+  
+  // prepare files for the output
+  ofstream fLuna("./luna.txt");
+  
   for (int i = 0; i < steps; i++)
     {
       myEvolution.evolution();
-      if ((i % 10) == 0)
+      if ((i % 200) == 0)
 	{
-	  cout << "T " << i * dt << " " << earth << endl;
-	  cout << "T " << i * dt << " " << moon << endl;
+	  fLuna << moon.getPosition() - earth.getPosition() << endl;
 	} 
     }
   
